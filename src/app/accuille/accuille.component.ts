683a25a972cdbad98994dfbc1416edaf0bfd0295
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-31 22:28:24 +0000

    add tailwindcss

---
+++ src/app/accuille/accuille.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accuille',
+  standalone: true,
+  imports: [],
+  templateUrl: './accuille.component.html',
+  styleUrl: './accuille.component.css'
+})
+export class AccuilleComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccuilleComponent } from './accuille/accuille.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AccuilleComponent ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
