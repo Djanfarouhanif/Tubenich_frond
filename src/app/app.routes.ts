199b04b1b6d1dcc68dbc88ceb92679161ca0188a
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-02 16:52:52 +0000

    add forms component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AccuilleComponent } from './accuille/accuille.component';
+import { FormsComponent } from './forms/forms.component';
 import path from 'path';
 
 export const routes: Routes = [
     {path:'' , redirectTo:'home', pathMatch:'full'},
     {path: 'home', component: AccuilleComponent},
+    {path: 'forms', component: FormsComponent}
 ];
