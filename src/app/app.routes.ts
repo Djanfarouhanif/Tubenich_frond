4c8833b971fb4634835ff3fd41f2d6e1341229c6
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-05 23:05:50 +0000

    form validations

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { AccuilleComponent } from './accuille/accuille.component';
 import { FormsComponent } from './forms/forms.component';
-import path from 'path';
+
 import { LoginComponent } from './login/login.component';
+import { VideosComponent } from './videos/videos.component';
 
 export const routes: Routes = [
     {path:'' , redirectTo:'home', pathMatch:'full'},
     {path: 'home', component: AccuilleComponent},
     {path: 'forms', component: FormsComponent},
-    {path: 'login', component: LoginComponent}
+    {path: 'login', component: LoginComponent},
+    {path: 'videos', component: VideosComponent}
 ];
--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FormGroup, FormControl, Validator, FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+
+
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FormsModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+    myForm!: FormGroup
 
+    constructor(){
+      this.myForm = new FormGroup({
+          username: new FormControl(''),
+          email: new FormControl(''),
+          password1 : new FormControl(''),
+          password: new FormControl(''),
+          choise: new FormControl(''),
+          checkbox : new FormControl('')
+      });
+    }
+    onSubmit(form:NgForm){
+      if(form.valid){
+          
+        console.log(this.myForm.value)
+      }
+      else{
+        console.log('form not valid')
+      }
+    };
 }
