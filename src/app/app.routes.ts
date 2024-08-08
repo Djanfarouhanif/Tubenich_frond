cf38e7a1fea3ac22d530fe26118ed33df5b4619c
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-08 15:44:59 +0000

    video page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { FormsComponent } from './forms/forms.component';
 
 import { LoginComponent } from './login/login.component';
 import { VideosComponent } from './videos/videos.component';
+import { VideoComponent } from './video/video.component';
 
 export const routes: Routes = [
     {path:'' , redirectTo:'home', pathMatch:'full'},
     {path: 'home', component: AccuilleComponent},
     {path: 'forms', component: FormsComponent},
     {path: 'login', component: LoginComponent},
-    {path: 'videos', component: VideosComponent}
+    {path: 'videos', component: VideosComponent},
+    {path: 'video', component: VideoComponent}
 ];
--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { ProfileComponent } from '../profile/profile.component';
 
-
 @Component({
-  selector: 'app-videos',
+  selector: 'app-video',
   standalone: true,
   imports: [ProfileComponent],
-  templateUrl: './videos.component.html',
-  styleUrl: './videos.component.css'
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.css'
 })
-export class VideosComponent {
+export class VideoComponent {
 
 }
