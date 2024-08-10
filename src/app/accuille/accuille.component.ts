e79eb1b8cef2a77a63baf40422c259871a076954
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-10 08:12:38 +0000

    content review

--- src/app/accuille/accuille.component.ts
+++ src/app/accuille/accuille.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router'
 import { NavbarComponent } from '../navbar/navbar.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-accuille',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './accuille.component.html',
   styleUrl: './accuille.component.css'
 })
