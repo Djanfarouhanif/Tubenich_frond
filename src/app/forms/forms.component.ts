513c849d883176b3f7dc018c0302abdfbcac7384
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-12 22:25:10 +0000

    add the http client

--- src/app/forms/forms.component.ts
+++ src/app/forms/forms.component.ts
@@ -1,20 +1,23 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
+import { ApiService } from '../api.service';
 
 import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [NavbarComponent, ReactiveFormsModule],
+  imports: [NavbarComponent, ReactiveFormsModule, HttpClientModule],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.css'
 })
 export class FormsComponent {
+   
     myForm!: FormGroup
-
-    constructor(private route:Router){
+  //Pour verifier la validation des information en d'envoyer
+    constructor(private route:Router, private apiservice: ApiService){
       this.myForm = new FormGroup({
           username: new FormControl('', Validators.required),
           email: new FormControl('', [Validators.email, Validators.required]),
@@ -24,6 +27,7 @@ export class FormsComponent {
           checkbox : new FormControl('', Validators.required)
       });
     }
+    //fonction pour la validation du formular une foie clicker
     onSubmit(){
       if(this.myForm.valid){
           
@@ -34,4 +38,27 @@ export class FormsComponent {
         console.log('form not valid')
       }
     };
+
+    //Fonction pour envoyer les donner via l'api
+    send(){
+      const data = {
+        'user': {
+          'username': 'irzzz',
+          'password': 'DDDDDDDDDDDDDDDDD',
+          'email': 'hs@gmail.com',
+          'last_name' : 'tode'
+        },
+        'langage': 'python'
+      }
+      this.apiservice.postData(data).subscribe(
+        response =>{
+          console.log(response);
+        },
+        error =>{
+          console.log(error);
+        }
+      );
+   
+    };
+      
 }
