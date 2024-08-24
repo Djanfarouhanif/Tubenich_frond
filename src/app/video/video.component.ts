56ee3a01aad218068f166a79235fef539b981513
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-08-24 20:01:53 +0000

    update of url for play automaticaly video on you clike our link

--- src/app/video/video.component.ts
+++ src/app/video/video.component.ts
@@ -3,7 +3,7 @@ import { ProfileComponent } from '../profile/profile.component';
 import { Router } from '@angular/router';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from '@angular/router';
-
+import { DomSanitizer, SafeResourceUrl} from '@angular/platform-browser'
 
 
 @Component({
@@ -17,14 +17,19 @@ import { ActivatedRoute } from '@angular/router';
 export class VideoComponent implements OnInit{
     videoId: string | null = '';
     title: string | null = "";
-    constructor(private router:Router, private apiservice: ApiService, private route:ActivatedRoute ){}
+    videoUrl!: SafeResourceUrl; 
+    constructor(private router:Router, private apiservice: ApiService, private route:ActivatedRoute, private sanitizer: DomSanitizer){}
 
   ngOnInit(){
+    
+    
     //recuper l'id du video 
     this.videoId = this.route.snapshot.paramMap.get('id');
     console.log('videoId', this.videoId);
 
     if(this.videoId){
+      //securiser lien de l'url youtube
+      this.videoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${this.videoId}?autoplay=1&rel=0`)
       this.apiservice.getVideo(this.videoId).subscribe(
         response =>{
           
